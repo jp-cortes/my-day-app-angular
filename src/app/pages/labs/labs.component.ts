058218251069f54407240cb97df546981a9adf9e
JP <alias99002@outlook.com>
2023-11-18 11:43:34 -0500

    creating a signal

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -33,11 +33,11 @@ export class LabsComponent {
   }
 
 
-  newAge = '';
+  newAge = signal('');
 
   changeHandler(event: Event) {
     const elementInput = event.target as HTMLInputElement
-    this.newAge = elementInput.value;
+    this.newAge.set(elementInput.value);
 
   }
 
