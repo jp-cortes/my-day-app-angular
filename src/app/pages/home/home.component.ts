3501ca20193be6a018c80fafd7bd0af2d9314608
JP <alias99002@outlook.com>
2023-11-19 12:01:28 -0500

    use of ngFor for objects

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../models/task.model'
 
 @Component({
   selector: 'app-home',
@@ -9,18 +10,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-    'Todo 1',
-    'Todo 2',
-    'Todo 3',
+  tasks = signal<Task[]>([
+   {
+    id: Date.now(),
+    title:'Learn Angular',
+    completed: false,
+   },
   ]);
 
-  addTask(event: Event) {
+_getTask() {
+  return this.tasks()
+}
+  changeHandler(event: Event) {
     const elementInput = event.target as HTMLInputElement;
     const newTask = elementInput.value;
-    this.tasks.update((tasks) => [...tasks, newTask]);
+    this.addTask(newTask)
 
   }
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false
+    }
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  markCompleted(id: number) {
+    this.tasks.update((tasks) => tasks.map((task) =>
+    task.id === id ? { ...task, completed: !task.completed } : task)
+    )
+    console.log(this._getTask())
+  }
+
   deleteTask(index: number) {
     this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
   }
