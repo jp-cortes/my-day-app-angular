93c7290e9a5ee39681c6aa893ccd7be29feab9f9
JP <alias99002@outlook.com>
2023-11-22 20:03:40 -0500

    implementing editing mode

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,58 +9,102 @@ import { ThisReceiver } from '@angular/compiler';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   tasks = signal<Task[]>([
-   {
-    id: Date.now(),
-    title:'Learn Angular',
-    completed: false,
-   },
+    {
+      id: Date.now(),
+      title: 'Learn Angular',
+      completed: false,
+    },
   ]);
 
   newTaskCtrl = new FormControl('', {
     nonNullable: true,
-    validators: [
-      Validators.required,
-    ]
-  })
+    validators: [Validators.required],
+  });
 
-getTask() {
-  return this.tasks()
-}
+  getTask() {
+    return this.tasks();
+  }
+
   changeHandler() {
-
-if(this.newTaskCtrl.valid) {
-  const value = this.newTaskCtrl.value.trim();
-  if(value !== '') {
-    this.addTask(value);
-    this.newTaskCtrl.setValue('');
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value.trim();
+      if (value !== '') {
+        this.addTask(value);
+        this.newTaskCtrl.setValue('');
+      }
+    }
   }
-}
 
-  }
   addTask(title: string) {
     const newTask = {
       id: Date.now(),
       title,
-      completed: false
-    }
+      completed: false,
+    };
     this.tasks.update((tasks) => [...tasks, newTask]);
   }
 
   markCompleted(id: number) {
-    this.tasks.update((tasks) => tasks.map((task) =>
-    task.id === id ? { ...task, completed: !task.completed } : task)
-    )
-    console.log(this.getTask())
+    this.tasks.update((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            completed: !task.completed,
+          };
+        }
+        return task
+      })
+    );
   }
 
+  updateTask(id: number) {
+    this.tasks.update((tasks) =>
+      tasks.map((task) => {
+        if (task.id === id) {
+          return {
+            ...task,
+            editing: true,
+          };
+        }
+        return {
+          ...task,
+          editing: false,
+        };
+      })
+    );
+  }
+
+  updateTaskHandler(id: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.tasks.update((tasks) =>
+      tasks.map((task) => {
+        const updatedTitle = input.value.trim()
+        if (task.id === id && updatedTitle !== '') {
+          return {
+            ...task,
+            title: updatedTitle ,
+            editing: false,
+          };
+        }
+        return task;
+      })
+    );
+  }
+
+
   deleteTask(index: number) {
-    this.tasks.update((tasks) => tasks.filter((task, position) => position !== index))
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
   }
   clearCompletedTask() {
-    this.tasks.update((tasks) => tasks.filter((task) => task.completed === false))
+    this.tasks.update((tasks) =>
+      tasks.filter((task) => task.completed === false)
+    );
   }
 }
